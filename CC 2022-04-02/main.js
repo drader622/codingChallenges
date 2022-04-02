45bfba3ecb2e80ac963b8d47a13791f0208df07b
drader622 <drader0827@gmail.com>
2022-04-02 17:06:35 -0400

    Add coding challenge for 04-02-2022

--- CC 2022-04-02/main.js
+++ CC 2022-04-02/main.js
@@ -7,6 +7,6 @@
 // Use reverse() to reverse the array
 let reverseList = list => list.reverse();
 
-//Tests
+//Test
 console.log(reverseList([1,2,3,4]), [4,3,2,1])
 console.log(reverseList([3,1,5,4]), [4,5,1,3]);
