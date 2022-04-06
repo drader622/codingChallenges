265f2cf11b044b33bc11e3a8a31c7a17e45d9b28
drader622 <drader0827@gmail.com>
2022-04-06 16:48:22 -0400

    Add coding challenge for 04-06-2022

---
+++ CC 2022-04-06/main.js
@@ -0,0 +1,19 @@
+// This is a spin off of my first kata.
+
+// You are given a string containing a sequence of character sequences separated by commas.
+
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+function array(arr){
+  return (arr.length >= 2) ? arr.split(',').slice(1, -1).join(' ') : null;
+}
+
+//Tests
+console.log(array('1,2,3'), '2');
+console.log(array('1,2,3,4'), '2 3');
+console.log(array('1,2,3,4,5'), '2 3 4');
+console.log(array(''), null);
+console.log(array('1'), null);
+console.log(array('1,2'), null);
