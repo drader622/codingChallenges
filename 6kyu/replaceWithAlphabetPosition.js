05f0e03749f4f5fa29f8b545cbdccb5fec47e595
drader622 <drader0827@gmail.com>
2022-06-01 16:11:42 -0400

    added coding challenge for 06-01-22

---
+++ 6kyu/replaceWithAlphabetPosition.js
@@ -0,0 +1,18 @@
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    return text.split('').map(letter => {
+        if ((/[a-zA-Z]/).test(letter)) {
+            for (let i = 0; i < alphabet.length; i++) {
+                if (letter.toLowerCase() === alphabet[i]) {
+                    return i + 1;
+                }
+            }
+        }
+    }).filter(x => x !== undefined).join(' ');
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+console.log(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
