f359f2cc7e3c09bf2497cd5295d2ba988950912c
Danielle Rader <drader0827@gmail.com>
2022-10-01 20:02:08 -0400

    added coding challenge from 09-29-2022

--- 7kyu/onesAndZeros.js
+++ 7kyu/onesAndZeros.js
@@ -1,15 +1,15 @@
 const binaryArrayToNumber = arr => {
-    let posValue = 8,
+    let posValue = arr.length * 2,
         total = 0;
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === 1) total += posValue;
         posValue /= 2;
     }
     return total;
-
 };
 
 console.log(binaryArrayToNumber([0, 0, 0, 1]), 1);
 console.log(binaryArrayToNumber([0, 0, 1, 0]), 2);
 console.log(binaryArrayToNumber([1, 1, 1, 1]), 15);
 console.log(binaryArrayToNumber([0, 1, 1, 0]), 6);
+console.log(binaryArrayToNumber([0, 1, 0, 0, 1, 0]), 18);
