f9e9e14e7e59dd38129e3515ce7fb8de2a51c579
Danielle Rader <drader0827@gmail.com>
2024-01-30 23:29:05 -0500

    fixed bug to catch no array passed

--- 8kyu/sumWithoutHighestAndLowestNumber.js
+++ 8kyu/sumWithoutHighestAndLowestNumber.js
@@ -11,7 +11,7 @@
 // If an empty value(null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
 
 function sumArray(array) {
-    if (array === null || array.length < 3)
+    if (array === null || array.length < 3 || !array)
         return 0;
     else {
         let sortedArr = array.sort((a, b) => a - b);
